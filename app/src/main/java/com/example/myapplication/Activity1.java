d36c13137d0a22341714df099623356781aae34f
Nash <namansharma18899@gmail.com>
2019-04-16 19:22:44 +0530

    Finally wrote the code for location XD

--- app/src/main/java/com/example/myapplication/Activity1.java
+++ app/src/main/java/com/example/myapplication/Activity1.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -8,21 +10,24 @@ import android.widget.TextView;
 
 public class Activity1 extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
         TextView tv2=(TextView) findViewById(R.id.tv2);
         TextView tv=(TextView) findViewById(R.id.tv);
-        if(getIntent().hasExtra("Extras")) {
-          //  String g=(getIntent().getExtras().getString("com.example.myapplication.Extras"));
-            Intent iin= getIntent();
-            Bundle b = iin.getExtras();
-            if(b!=null)
-            {
-                String j =(String) b.get("Extras");
-                tv2.setText(j);
-            }
-        }
+      /* if(getIntent().hasExtra("Extras")) {
+                //  String g=(getIntent().getExtras().getString("com.example.myapplication.Extras"));
+                Intent iin= getIntent();
+                Bundle b = iin.getExtras();
+                if(b!=null)
+                {
+                    //String s[] =(String[]) b.getStringArray("Extras");
+                    String st= (String) b.getString("Extras");
+                    // double d = (double) b.getDouble("Extras");
+                    tv2.setText(st);
+                }
+        } */
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +17,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button buta=(Button) findViewById(R.id.buta);
         final EditText tv=(EditText) findViewById(R.id.tv);
+        final Location location=null;
         buta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String s=tv.getText().toString();
-                Intent intero=new Intent(getApplicationContext(),Activity1.class);
+                Intent intero=new Intent(getApplicationContext(),mapart.class);
                 intero.putExtra("Extras",s);
                 startActivity(intero);
                 //war
